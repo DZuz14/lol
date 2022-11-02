b6fed924d4f430a7528428a40b836de120a380d7
4ch1k0 <103787505+4ch1k0@users.noreply.github.com>
2022-11-02 18:08:56 +0100

    accordion in faq page

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,6 @@
-import Head from 'next/head';
-import Navbar from './Navbar';
-import Footer from './Footer';
+import Head from "next/head";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 export default function Layout({ children }) {
   return (
@@ -12,6 +12,21 @@ export default function Layout({ children }) {
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
           crossOrigin="anonymous"
         />
+        <script
+          src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+          integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+          crossorigin="anonymous"
+        ></script>
+        <script
+          src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+          integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+          crossorigin="anonymous"
+        ></script>
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+          integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+          crossorigin="anonymous"
+        ></script>
       </Head>
 
       <Navbar />
--- pages/faq.js
+++ pages/faq.js
@@ -39,62 +39,217 @@ export default function faq() {
       </section>
       <div>
         <section className="padding-lg">
-          <Accordion defaultActiveKey="0" flush>
-            <Accordion.Item eventKey="0">
-              <Accordion.Header>Is my data safe?</Accordion.Header>
-              <Accordion.Body>
-                One of the best designers that turns the client pain points and
-                requests into magnificent designs. Aware of all the aspect that
-                should be considered to fulfill the requirements in high-quality
-                implementation as an expected end result. Has patience while
-                receiving and confirming the requirements, conducting the wire
-                frames and style guides and also creates great portoflios for
-                their projects.
-              </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="1">
-              <Accordion.Header>
-                How does the 30-day free software trial work?
-              </Accordion.Header>
-              <Accordion.Body>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="2">
-              <Accordion.Header>
-                What makes LessAccounting better than the rest?
-              </Accordion.Header>
-              <Accordion.Body>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="3">
-              <Accordion.Header>
-                What makes LessAccounting better than the rest?
-              </Accordion.Header>
-              <Accordion.Body>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </Accordion.Body>
-            </Accordion.Item>
-          </Accordion>
+          <div
+            className={classNames("mx-auto", styles.accordion)}
+            id="accordionExample"
+          >
+            <div className={styles.firstCard}>
+              <div className="card-header" id="headingOne">
+                <h2 className="mb-0">
+                  <button
+                    className="btn btn-block text-left"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#collapseOne"
+                    aria-expanded="true"
+                    aria-controls="collapseOne"
+                  >
+                    Is my data safe?
+                  </button>
+                </h2>
+              </div>
+
+              <div
+                id="collapseOne"
+                className="collapse show"
+                aria-labelledby="headingOne"
+                data-parent="#accordionExample"
+              >
+                <div className="card-body pb-5 pt-5">
+                  One of the best designers that turns the client pain points
+                  and requests into magnificent designs. Aware of all the aspect
+                  that should be considered to fulfill the requirements in
+                  high-quality implementation as an expected end result. Has
+                  patience while receiving and confirming the requirements,
+                  conducting the wire frames and style guides and also creates
+                  great portoflios for their projects.
+                </div>
+              </div>
+            </div>
+            <div className={styles.card}>
+              <div className="card-header" id="headingTwo">
+                <h2 className="mb-0">
+                  <button
+                    className="btn btn-block text-left collapsed"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#collapseTwo"
+                    aria-expanded="false"
+                    aria-controls="collapseTwo"
+                  >
+                    How does the 30-day free software trial work?
+                  </button>
+                </h2>
+              </div>
+              <div
+                id="collapseTwo"
+                class="collapse"
+                aria-labelledby="headingTwo"
+                data-parent="#accordionExample"
+              >
+                <div class="card-body">
+                  Some placeholder content for the second accordion panel. This
+                  panel is hidden by default.
+                </div>
+              </div>
+            </div>
+            <div class={styles.card}>
+              <div class="card-header" id="headingThree">
+                <h2 class="mb-0">
+                  <button
+                    class="btn btn-block text-left collapsed"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#collapseThree"
+                    aria-expanded="false"
+                    aria-controls="collapseThree"
+                  >
+                    What happens after my free trial?
+                  </button>
+                </h2>
+              </div>
+              <div
+                id="collapseThree"
+                class="collapse"
+                aria-labelledby="headingThree"
+                data-parent="#accordionExample"
+              >
+                <div class="card-body">
+                  And lastly, the placeholder content for the third and final
+                  accordion panel. This panel is hidden by default.
+                </div>
+              </div>
+              <div class={styles.card}>
+                <div class="card-header" id="headingFour">
+                  <h2 class="mb-0">
+                    <button
+                      class="btn btn-block text-left collapsed"
+                      type="button"
+                      data-toggle="collapse"
+                      data-target="#collapseFour"
+                      aria-expanded="true"
+                      aria-controls="collapseFour"
+                    >
+                      What makes LessAccounting better than the rest?
+                    </button>
+                  </h2>
+                </div>
+
+                <div
+                  id="collapseFour"
+                  class="collapse"
+                  aria-labelledby="headingFour"
+                  data-parent="#accordionExample"
+                >
+                  <div class="card-body">
+                    Some placeholder content for the first accordion panel. This
+                    panel is shown by default, thanks to the <code>.show</code>{" "}
+                    class.
+                  </div>
+                </div>
+
+                <div class={styles.card}>
+                  <div class="card-header" id="headingFive">
+                    <h2 class="mb-0">
+                      <button
+                        class="btn btn-block text-left collapsed"
+                        type="button"
+                        data-toggle="collapse"
+                        data-target="#collapseFive"
+                        aria-expanded="true"
+                        aria-controls="collapseFive"
+                      >
+                        Is my data safe?
+                      </button>
+                    </h2>
+                  </div>
+
+                  <div
+                    id="collapseFive"
+                    class="collapse"
+                    aria-labelledby="headingFivw"
+                    data-parent="#accordionExample"
+                  >
+                    <div class="card-body">
+                      Some placeholder content for the first accordion panel.
+                      This panel is shown by default, thanks to the{" "}
+                      <code>.show</code> class.
+                    </div>
+                  </div>
+                </div>
+
+                <div class={styles.card}>
+                  <div class="card-header" id="headingSix">
+                    <h2 class="mb-0">
+                      <button
+                        class="btn btn-block text-left collapsed"
+                        type="button"
+                        data-toggle="collapse"
+                        data-target="#collapseSix"
+                        aria-expanded="true"
+                        aria-controls="collapseSix"
+                      >
+                        How does the 30-day free software trial work?
+                      </button>
+                    </h2>
+                  </div>
+
+                  <div
+                    id="collapseSix"
+                    class="collapse"
+                    aria-labelledby="headingSix"
+                    data-parent="#accordionExample"
+                  >
+                    <div class="card-body">
+                      Some placeholder content for the first accordion panel.
+                      This panel is shown by default, thanks to the{" "}
+                      <code>.show</code> class.
+                    </div>
+                  </div>
+                </div>
+
+                <div class={styles.card}>
+                  <div class="card-header" id="headingSeven">
+                    <h2 class="mb-0">
+                      <button
+                        class="btn btn-block text-left collapsed"
+                        type="button"
+                        data-toggle="collapse"
+                        data-target="#collapseSeven"
+                        aria-expanded="true"
+                        aria-controls="collapseSeven"
+                      >
+                        What happens after my free trial?
+                      </button>
+                    </h2>
+                  </div>
+
+                  <div
+                    id="collapseSeven"
+                    class="collapse"
+                    aria-labelledby="headingSeven"
+                    data-parent="#accordionExample"
+                  >
+                    <div class="card-body">
+                      Some placeholder content for the first accordion panel.
+                      This panel is shown by default, thanks to the{" "}
+                      <code>.show</code> class.
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </section>
       </div>
     </div>
